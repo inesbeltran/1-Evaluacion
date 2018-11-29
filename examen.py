059829694708bfef867b890b015d8c6221d1ac8f
inesbeltran <43232795+inesbeltran@users.noreply.github.com>
2018-11-29 19:39:52 +0100

    Add files via upload

---
+++ examen.py
@@ -0,0 +1,40 @@
+def programa():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    num3 = float(input("Enter third number: "))
+    num4 = float(input("Enter fourth number: "))
+    num5 = float(input("Enter fifth number: "))
+    num6 = float(input("Enter sixth number: "))
+    num7 = float(input("Enter seventh number: "))
+    num8 = float(input("Enter eight number: "))
+    num9 = float(input("Enter nineth number: "))
+    num10 = float(input("Enter ten number: "))
+    for i in range (1,100):
+        if(num1<=i):
+            print num1
+        elif(num2<=i):
+            print num2
+        elif(num3<=i):
+            print num3
+        elif(num4<=i):
+            print num4
+        elif(num5<=i):
+            print num5
+        elif(num6<=i):
+            print num6
+        elif(num7<=i):
+            print num7
+        elif(num8<=i):
+            print num8
+        elif(num9<=i):
+            print num9
+        elif(num10<=i):
+            print num10
+        
+        
+        
+
+programa()
+
+
+ 
